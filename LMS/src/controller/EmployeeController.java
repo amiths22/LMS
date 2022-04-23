22ae94ed40de0d41ab9d18a23493af1438dac3f2
amith <amith@DESKTOP-4EPK2KM>
2022-04-23 03:44:07 -0500

    Added apply leave function

--- LMS/src/controller/EmployeeController.java
+++ LMS/src/controller/EmployeeController.java
@@ -1,19 +1,23 @@
 package controller;
 import java.awt.TextArea;
 import java.io.IOException;
+import java.sql.Date;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.util.Calendar;
 
 import javax.swing.JComboBox;
+import javax.swing.JOptionPane;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextArea;
-
+import javafx.event.ActionEvent;
 import model.DBConnect;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -28,13 +32,18 @@ import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 import javafx.stage.Stage;
+import java.time.DayOfWeek;
+import java.time.temporal.ChronoUnit;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 public class EmployeeController {
 	
 	private Stage stage;
 	private Scene scene;
 	private Parent root;
 	
-	ObservableList<String> lstUserType = FXCollections.observableArrayList("Annual", "Casual", "Sick", "Maternity", "Paternity");
+	ObservableList<String> listLeaveType = FXCollections.observableArrayList("Annual", "Casual", "Sick", "Maternity", "Paternity");
 
     @FXML
     private JFXTextArea reasonbox;
@@ -63,27 +72,54 @@ public class EmployeeController {
     @FXML
 	private void initialize()
 	{
-		combobox.setItems(lstUserType);
+		combobox.setItems(listLeaveType);
 		dbConnect = new DBConnect();
 	}
     
-/*    private void onApplyLeave(ActionEvent event) throws IOException
+    public void onapplyleave(ActionEvent event) throws IOException
     {
     	try {
     		LocalDate dleavefrom = leavefrom.getValue();
     		LocalDate dleaveto = leaveto.getValue();
     		String leavetype = combobox.getValue();
-    		String sreason = reasonbox.getText();
+    		String scomments = reasonbox.getText();
+    		int nod=countLeaveDays(dleavefrom,dleaveto);
+    		int leavetypeint=-1;
     		
-    		Statement = dbConnect.connect().createStatement();
+    		switch(leavetype) {
+    		case "Annual": leavetypeint=1;
+    			break;
+    		case "Casual":leavetypeint=2;
+    			break;
+    		case "Sick": leavetypeint=3;
+    			break;
+    		case "Maternity": leavetypeint=4;
+    			break;
+    		case "Paternity": leavetypeint=5;
+    			break;
+    		}
     		
-    		//string sql = "INSERT INTO "
+    		System.out.println("leavedays"+nod);
+    		System.out.println("Empid"+sUsername);
+    		
+    		Statement = dbConnect.getconnection().createStatement();
+    		
+    		String sql = "INSERT into leaverecords (emp_id,fromdate,todate,nod,type,comments) VALUES"
+    				+ " (' "+sUsername+" ',' "+dleavefrom+" ','"+dleaveto+"',' "+nod+" ',' "+leavetypeint+" ','"+scomments+"')";
+    		
+    		int con = Statement.executeUpdate(sql);
+			if (con > 0) 
+			{
+				JOptionPane.showMessageDialog(null,"Leave applied for "+nod+" days");
+			}
+    		
     	}
     	catch(SQLException e) {
     		e.printStackTrace();
     	}
     	
-    }*/
+    	
+    }
    public void onlogout(ActionEvent event) throws IOException
 	{
 		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/LoginView.fxml"));
@@ -92,6 +128,37 @@ public class EmployeeController {
 		scene = new Scene(root, 600, 400);
 		stage.setScene(scene);
 		stage.show();
+    }
+    
+    
+
+    private static int countLeaveDays(final LocalDate startDate,
+            final LocalDate endDate
+            )
+    {
+        // Validate method arguments
+        if (startDate == null || endDate == null) {
+            throw new IllegalArgumentException("Invalid method argument(s) to countBusinessDaysBetween (" + startDate+ "," + endDate + ")");
+        }
+
+        // Predicate 1: Is a given date is a holiday
+       // Predicate<LocalDate> isHoliday = date -> holidays.isPresent() 
+             //   && holidays.get().contains(date);
+
+        // Predicate 2: Is a given date is a weekday
+        Predicate<LocalDate> isWeekend = date -> date.getDayOfWeek() == DayOfWeek.SATURDAY
+                || date.getDayOfWeek() == DayOfWeek.SUNDAY;
+
+        // Get all days between two dates
+        long daysBetween = ChronoUnit.DAYS.between(startDate, endDate);
+
+        // Iterate over stream of all dates and check each day against any weekday or
+        // holiday
+        return Stream.iterate(startDate, date -> date.plusDays(1))
+                .limit(daysBetween)
+                .filter(isWeekend.negate())
+                .collect(Collectors.toList()).size()+1;
+        
     }
 
 }
